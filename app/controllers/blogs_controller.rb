ec4d09d5980d0615fe7713c00402b1cbbd05c232
vagrant <vagrant@localhost.localdomain>
2020-03-25 06:57:35 +0000

    [Update]4-6

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -35,6 +35,6 @@ class BlogsController < ApplicationController
 
   private
     def blog_params
-      params.require(:blog).permit(:title, :category, :body)
+      params.require(:blog).permit(:title, :category, :body,)
     end
 end
